6953fc66c4e096463b8b6d1ff4c0537f9bf39618
SanaaAtif <162082729+SanaaAtif@users.noreply.github.com>
2026-07-06 16:38:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-iimport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/portofolio/",
 })
